b121d1b6d31f3fab25078c7ed817a5074b91497c
tmacek1 <tomislav.macek4@gmail.com>
2019-01-03 17:50:10 +0000

    inital commit

---
+++ src/deactivate.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'auth_check.php';
+include 'db.php';
+
+$id = $_GET['id'];
+$sql = "update admins set status = 0 where id = '$id'";
+
+$val = $db->query($sql);
+if($val){
+	            header('location: admin_panel.php');
+}
+?>
