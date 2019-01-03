099031a5daed5d35c227545a67ed5f4d137bcf2f
tmacek1 <tomislav.macek4@gmail.com>
2019-01-03 18:13:20 +0000

    configuration file added

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$username = 'mica';
+$db_pass = 'dSHmiA0X';
+$db_name = 'crud_db';
+
+?>
---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'config.php';
+
+$db = new Mysqli;
+$db->connect("$host","$username","$db_pass","$db_name");
+
+if(!$db) {
+die('Could not connect to DB:' .mysql_error());
+}
+?>
