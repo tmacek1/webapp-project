38f39e363d83a7c59d3b039df657e807abe7be61
tmacek1 <tomislav.macek4@gmail.com>
2019-01-09 21:41:30 +0100

    fixing minor issues

--- src/players.php
+++ src/players.php
@@ -3,6 +3,7 @@
 include 'auth_check.php';
 include 'xml_functions.php';
 include 'db.php';
+include 'login_menu.php';
 
 $sql = "select * from users where activated = 1";
 $rows = $db->query($sql);
@@ -11,32 +12,17 @@ $rows = $db->query($sql);
 
 <!DOCTYPE html>
 <html>
-   <head>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-   </head>
-
    <body>
-
       <div class="conatiner">
          <div class="row">
             <div class="col-md-5"></div>
             <h1 class="text-success">Player database</h1>
          </div>
-
-
          <div class="col-12 col-sm-6 col-md-6">
-         <div data-toggle="modal" data-target="#myModal">
-          <form>
-          <label class="btn btn-primary">Add new user</label>
-         </form>
+          <div data-toggle="modal" data-target="#myModal">
+            <label class="btn btn-primary">Add new user</label>
+          </div>
          </div>
-
-
-
          <!-- Modal -->
          <div id="myModal" class="modal fade" role="dialog">
             <div class="modal-dialog">
@@ -94,33 +80,28 @@ $rows = $db->query($sql);
                <td class="col-md-0"><?php echo $row['registration_time']?>
 
                <?php
-
                if ($_SESSION['role'] == 'admin') {
                  echo '<td><a href="update.php?id='.$row["id"].'" class="btn btn-success">Edit</a></td>
                        <td><a href="delete.php?id='.$row["id"].'" class="btn btn-danger">Delete</a></td>';
                }
-
                ?>
-
 
             </tr>
             <?php endwhile; ?>
          </tbody>
       </table>
+       <div class="col-12 col-sm-6 col-md-6">
+            <form method="post" action="players.php" enctype="multipart/form-data">
+               <input type="file" name="chooseFile" id="chooseFile">
+	            <label class="btn btn-success"><input id="submit" type="submit" style="display:none;">Submit XML</label>
+            </form>
+	    </div>
 
-	      <div class="col-12 col-sm-6 col-md-6">
-               <form method="post" action="players.php" enctype="multipart/form-data">
-
-                <input type="file" name="chooseFile" id="chooseFile">
-
-	        <label class="btn btn-success"><input id="submit" type="submit" style="display:none;">Submit XML</label>
-              </form>
-	     </div>
 <?php
 if ((strtolower($_SERVER['REQUEST_METHOD'])=='post') && (!empty($_FILES['chooseFile']['tmp_name']))){
 
     $file = $_FILES['chooseFile']['tmp_name'];
-    $schema = '/home/mica/html/xml_schema.xsd';
+    $schema = 'xml_schema.xsd';
     $ab = new DOMDocument;
 
     libxml_use_internal_errors(true);
@@ -129,7 +110,7 @@ if ((strtolower($_SERVER['REQUEST_METHOD'])=='post') && (!empty($_FILES['chooseF
     if ($ab->schemaValidate($schema)) {
 	    print "Imported XML is valid.\n";
     } else {
-	print "Imported XML is invalid.\n";
+	   print "Imported XML is invalid.\n";
 	return;
     }
 
@@ -148,17 +129,16 @@ if ((strtolower($_SERVER['REQUEST_METHOD'])=='post') && (!empty($_FILES['chooseF
         $duplicate = "select * from users where user_name = '$user_name'";
         $dupval = $db->query($duplicate);
 
-	if ($dupval->num_rows>0) {
-		echo "<script type='text/javascript'>alert('User already exists, please try again');document.location.href='players.php';</script>";
-		return;
-        }
-        else{
-          $sql = "insert into users (user_name, first_name, last_name, birth_place, birth_date) values ('$user_name','$first_name','$last_name','$birth_place', '$birth_date')";
-          $val = $db->query($sql);
-          array_push($userAray,$user_name);
-        }
+      if ($dupval->num_rows>0) {
+         echo "<script type='text/javascript'>alert('User already exists, please try again');document.location.href='players.php';</script>";
+         return;
+      } else {
+         $sql = "insert into users (user_name, first_name, last_name, birth_place, birth_date) values ('$user_name','$first_name','$last_name','$birth_place', '$birth_date')";
+         $val = $db->query($sql);
+         array_push($userAray,$user_name);
       }
-    }
+   }
+}
     $arrayP = implode(" ",$userAray);
     $message = "XML import finished successfully, new users added: ".$arrayP;
     echo "<script type='text/javascript'>alert('$message');document.location.href='players.php';</script>";
@@ -166,12 +146,12 @@ if ((strtolower($_SERVER['REQUEST_METHOD'])=='post') && (!empty($_FILES['chooseF
 ?>
 
 
-   <div>
-    <p align="center">
-    <button type="button" class="btn btn-info" role="button" onClick="document.location.href='welcome.php'" style="margin-left:auto;margin-right:auto;display:block;margin-top:22%;margin-bottom:0%">Homepage</button>
-    </p>
-   </div>
+<div>
+   <p align="center">
+   <button type="button" class="btn btn-info" role="button" onClick="document.location.href='welcome.php'" style="margin-left:auto;margin-right:auto;display:block;margin-top:22%;margin-bottom:0%">Homepage</button>
+   </p>
 </div>
+
 
 <div id="footer">
  <div class="container text-center">
@@ -183,7 +163,5 @@ if ((strtolower($_SERVER['REQUEST_METHOD'])=='post') && (!empty($_FILES['chooseF
  </div>
 </div>
 
-
-
-   </body>
+</body>
 </html>
